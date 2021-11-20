aaf224312c7ae2648cfc0509700a411d6d187a2a
Reva <revagupta95@gmail.com>
2021-11-20 09:01:41 -0600

    Text and Image preprocessing

--- Data_Processing.py
+++ Data_Processing.py
@@ -1,6 +1,10 @@
 import csv
 import string
-from pprint import pprint
+from PIL import Image
+import os
+import fnmatch
+import numpy as np
+import pandas as pd
 
 
 class DataProcessing:
@@ -10,6 +14,8 @@ class DataProcessing:
     Methods
     -------
     caption_reader(): read captions from the txt/csv file
+    clean_caption(): clean the captions
+    process_images(): change the image dimensions and store in data
     """
 
     def __init__(self):
@@ -18,6 +24,7 @@ class DataProcessing:
         """
         self.data = {}
         self.punctuations = string.punctuation
+        self.embedding_dictionary = {}
 
     def caption_reader(self):
         """
@@ -30,9 +37,8 @@ class DataProcessing:
             next(reader)  # ignore header line
             for row in reader:  # add data to dictionary
                 if row[0] not in self.data:
-                    self.data[row[0]] = list()
-                self.data[row[0]].append(self.clean_captions(row[1]))
-            pprint(self.data)
+                    self.data[row[0]] = {'captions': list()}
+                self.data[row[0]]['captions'].append(self.clean_captions(row[1]))
 
     def clean_captions(self, caption):
         """
@@ -41,12 +47,44 @@ class DataProcessing:
         :return: cleaned caption
         """
         word_list = caption.lower().split()  # tokenize
-        for punctuation in self.punctuations:
+        word_list = [word for word in word_list if len(word) > 1]  # remove hanging words
+        for punctuation in self.punctuations:  # remove punctuations
             if punctuation in word_list:
                 word_list.remove(punctuation)
         return ' '.join(word_list)
 
+    # def word_embedding(self):
+    #     """
+    #
+    #     """
+    #     glove_dir = "/Users/revagupta/Documents/UTD/Second Sem/CS-6375 ML/ML_Project/glove/glove.6B.200d.txt"
+    #     with open(glove_dir, encoding="utf-8", mode='r') as file: # open the file
+    #         reader = csv.reader(file)  # read the file
+    #         for row in reader:
+    #             print(row)
+    #             # row = row[0].split()
+    #             # print(row)
+    #             # word = row[0]
+    #             # coef = np.asarray(row[1:], dtype='float32')
+    #             # self.embedding_dictionary[word] = coef
+
+    def process_images(self):
+        """
+        Read the images, change its dimension and store in dictionary
+        """
+        # hardcoded path change in future
+        image_folder_path = "/Users/revagupta/Documents/UTD/Second Sem/CS-6375 ML/ML_Project/Images"
+        for file in os.listdir(image_folder_path):  # Pick list of images
+            if fnmatch.fnmatch(file, "*.jpg"):  # check if the file is a jpg file
+                image = Image.open(image_folder_path + "/" + file)  # opening the file
+                image = np.asarray(image)   # converting image into array
+                image_resize = np.resize(image, (224, 224, 3))  # reshaping the image
+                self.data[file]["image_vector"] = image_resize  # storing the image in the dictionary
+
 
 if __name__ == "__main__":
     data_processing = DataProcessing()
+    print("Processing Captions")
     data_processing.caption_reader()
+    print("Processing Images")
+    data_processing.process_images()
