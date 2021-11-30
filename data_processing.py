1cc83436c574c5ba0ae3b08c9980e5663ec7657d
Reva <revagupta95@gmail.com>
2021-11-29 23:21:45 -0600

    files hosted

--- data_processing.py
+++ data_processing.py
@@ -1,14 +1,14 @@
-import csv
 import string
 from PIL import Image
-import fnmatch
 import numpy as np
 from nltk.tokenize import RegexpTokenizer
 import torch
 from gensim.models import Word2Vec
-import glob
-from keras.utils import np_utils
+import requests
+from io import BytesIO
 
+
+# from keras.utils import np_utils
 
 class Processor:
     """
@@ -28,45 +28,40 @@ class Processor:
         self.data = {}
         self.punctuations = string.punctuation
         self.tokenizer = RegexpTokenizer(r'\w+')
-        self.glove_dir = "/Users/ayush/Downloads/glove/glove.6B.200d.txt"
-        self.caption_file_path = "/content/results.csv"
+        self.caption_file_path = \
+            "https://raw.githubusercontent.com/ayushgoel26/datasets/main/ImageProcessingData/captions.txt"
+        self.image_file_path = "https://raw.githubusercontent.com/ayushgoel26/datasets/main/ImageProcessingData/Images/"
         # self.word_embeddings = self.word_vec_reader()
         self.captions_list = []
         self.vocabulary = None
         self.model = None
 
-    def word_vec_reader(self):
-        print('Reading Glove Vectors')
-        glove_vecs = {"startseq": np.full(100, 1), "endseq": np.full(100, -1)}
-        with open(self.glove_dir, encoding="utf-8", mode='r') as file:  # open the file
-            for line in file.readlines():
-                line = line.replace("\n", "").split(" ")
-                glove_vecs[line[0]] = [float(i) for i in line[1:]]
-        return glove_vecs
+    # def word_vec_reader(self):
+    #     print('Reading Glove Vectors')
+    #     glove_vecs = {"startseq": np.full(100, 1), "endseq": np.full(100, -1)}
+    #     with open(self.glove_dir, encoding="utf-8", mode='r') as file:  # open the file
+    #         for line in file.readlines():
+    #             line = line.replace("\n", "").split(" ")
+    #             glove_vecs[line[0]] = [float(i) for i in line[1:]]
+    #     return glove_vecs
 
     def caption_reader(self):
         """
         read data into dictionary from txt/csv file
         """
         # hardcoded path change in future
-        with open(self.caption_file_path, mode='r') as file:  # open the file
-            reader = csv.reader(file)  # read the file
-            next(reader)  # ignore header line
-            for row in reader:  # add data to dictionary
-                # row[0] itself has all the fields hence need to split wrt '|'
-                # eg row[0] = 1000092795.jpg| 0| Two young guys with shaggy hair look at their hands while hanging out in the yard .
-                line = row[0].split('|')
-                # in some cases split doesnt create length 3 meaning bad data
-                if len(line) < 3:
-                    continue  # hence skipping
-                # extract 1000092795.jpg from row[0]
-                file_name = line[0]
-                caption = line[2]
-                if file_name not in self.data:
-                    self.data[file_name] = {'captions': list()}
-                word_list = self.clean_caption(caption)
-                self.captions_list.append(word_list)
-                self.data[file_name]['captions'].append(word_list)
+        page = requests.get(self.caption_file_path)
+        count = 0
+        for text in page.text.split('\n')[1:-1]:
+            line = text.split(',')
+            if line[0] not in self.data:
+                self.data[line[0]] = {'captions': list()}
+            word_list = self.clean_caption(line[1])
+            self.captions_list.append(word_list)
+            self.data[line[0]]['captions'].append(word_list)
+            count += 1
+            if count == 1500:
+                break
         self.word_to_vector()
         self.vectoriser()
 
@@ -79,7 +74,7 @@ class Processor:
         print("Word to Vector model ->", self.model)
         self.vocabulary = list(self.model.wv.vocab)
         print("Vocabulary length ->", len(self.vocabulary))
-        self.model.save('model.bin')    # save the word to vector model
+        self.model.save('model.bin')  # save the word to vector model
         # self.visualize_word_embedding()
 
     def visualize_word_embedding(self):
@@ -101,7 +96,8 @@ class Processor:
     def vectoriser(self):
         for key in self.data:
             for index in range(len(self.data[key]['captions'])):
-                self.data[key]['captions'][index] = [self.get_word_embedding(word) for word in self.data[key]['captions'][index]]
+                self.data[key]['captions'][index] = \
+                    [self.get_word_embedding(word) for word in self.data[key]['captions'][index]]
 
     def clean_caption(self, caption):
         """
@@ -131,36 +127,26 @@ class Processor:
         """
         Read the images, change its dimension and store in dictionary
         """
-        # hardcoded path change in future
-        image_folder_path = "/content/images/*.jpg"
-        # sample file path will be /content/images/24343.jpg
-        for file in glob.glob(image_folder_path):  # Pick list of images
-            # file will be path : /content/images/24343.jpg
-            if fnmatch.fnmatch(file, "*.jpg"):  # check if the file is a jpg file
-                image = Image.open(file)  # opening the file
-                image = np.asarray(image)   # converting image into array
-                # reshaping the image , d * h * w, small change instead of h * w * d
-                image_resize = np.resize(image, (3, 112, 112))
-                image_resize = np.true_divide(image_resize, 255)    # normalization : diving values by 255
-                # extract 24343.jpg from /content/images/24343.jpg
-                file_name = file.split('/')[3]
-                # file_name = (int)(id.split('.')[0])
-                # self.data[id] = {}
-                self.data[file_name] = {'image': (list())}
-                # key : 24343.jpg , 'images' will be image matrix
-                self.data[file_name]['image'].append(image_resize)  # storing the image in the dictionary
+        for key in self.data.keys():
+            response = requests.get(self.image_file_path + key)
+            image = Image.open(BytesIO(response.content))
+            image = np.asarray(image)  # converting image into array
+            # reshaping the image , d * h * w, small change instead of h * w * d
+            image_resize = np.resize(image, (3, 112, 112))
+            image_resize = np.true_divide(image_resize, 255)  # normalization : diving values by 255
+            self.data[key]['image'] = image_resize  # storing the image in the dictionary
 
-    def preprocess_data_mnist(self, x, y, limit):
-        """
-        This returns processed training data for MNIST
-        """
-        zero_index = np.where(y == 0)[0][:limit]
-        one_index = np.where(y == 1)[0][:limit]
-        all_indices = np.hstack((zero_index, one_index))
-        all_indices = np.random.permutation(all_indices)
-        x, y = x[all_indices], y[all_indices]
-        x = x.reshape(len(x), 1, 28, 28)
-        x = x.astype("float32") / 255
-        y = np_utils.to_categorical(y)
-        y = y.reshape(2, len(y))
-        return x, y
+    # def preprocess_data_mnist(self, x, y, limit):
+    #     """
+    #     This returns processed training data for MNIST
+    #     """
+    #     zero_index = np.where(y == 0)[0][:limit]
+    #     one_index = np.where(y == 1)[0][:limit]
+    #     all_indices = np.hstack((zero_index, one_index))
+    #     all_indices = np.random.permutation(all_indices)
+    #     x, y = x[all_indices], y[all_indices]
+    #     x = x.reshape(len(x), 1, 28, 28)
+    #     x = x.astype("float32") / 255
+    #     y = np_utils.to_categorical(y)
+    #     y = y.reshape(2, len(y))
+    #     return x, y
--- main.py
+++ main.py
@@ -1,71 +1,51 @@
-import numpy as np
 from data_processing import Processor
-import TrainData
 
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from google.colab import auth
-from oauth2client.client import GoogleCredentials
-
-"""getting captions and images from drive"""
-
-auth.authenticate_user()
-gauth = GoogleAuth()
-gauth.credentials = GoogleCredentials.get_application_default()
-drive = GoogleDrive(gauth)
-
-downloaded = drive.CreateFile({'id': "1ZyaOYHhL6jwzduBwDZgZLyAXlzqOBOnx"})  # get captions
-downloaded.GetContentFile('results.csv')
-
-downloaded = drive.CreateFile({'id': "1vDJYsj8HRcMVK67TbNqma2GddW9otC4m"})  # get images
-downloaded.GetContentFile('images.zip')
-
-dat = processor.data
-
-mxPoolKSize = 3
-mxPoolStride = 2
-mxPoolPadding = 1
-
-processedFeatures = {}
-
-'''Running CNN for test images'''
-input_dimensions = (3, 112, 112)
-for k in dat:
-    print('Creating features for ' + k)
-    if 'images' not in dat[k]: continue;
-    t = dat[k]['images']
-    t = np.asarray(t)
-    t = np.reshape(t, input_dimensions)
-
-    learning_rate = 0.01
-    t1 = convLayer1.forward(t)
-
-    t1 = np.reshape(t1, (t1.shape[1], t1.shape[2], t1.shape[0]))
-    t2 = mxPoolLayer1.forward(t1)
-    t2 = np.reshape(t2, (t2.shape[2], t2.shape[0], t2.shape[1]))
-
-    t3 = convLayer2.forward(t2)
-
-    t3 = np.reshape(t3, (t3.shape[1], t3.shape[2], t3.shape[0]))
-    t4 = mxPoolLayer2.forward(t3)
-    t4 = np.reshape(t4, (t4.shape[2], t4.shape[0], t4.shape[1]))
-
-    t5 = convLayer3.forward(t4)
-
-    t5 = np.reshape(t5, (t5.shape[1], t5.shape[2], t5.shape[0]))
-    t6 = mxPoolLayer3.forward(t5)
-    t6 = np.reshape(t6, (t6.shape[2], t6.shape[0], t6.shape[1]))
-
-    t7 = convLayer4.forward(t6)
-
-    t7 = np.reshape(t7, (t7.shape[1], t7.shape[2], t7.shape[0]))
-    t8 = mxPoolLayer4.forward(t7)
-    t8 = np.reshape(t8, (t8.shape[2], t8.shape[0], t8.shape[1]))
-
-    t9 = convLayer5.forward(t8)
-
-    t_final = flatten.forward(t9)
-    processedFeatures[k] = t_final
+# dat = processor.data
+#
+# mxPoolKSize = 3
+# mxPoolStride = 2
+# mxPoolPadding = 1
+#
+# processedFeatures = {}
+#
+# '''Running CNN for test images'''
+# input_dimensions = (3, 112, 112)
+# for k in dat:
+#     print('Creating features for ' + k)
+#     if 'images' not in dat[k]: continue;
+#     t = dat[k]['images']
+#     t = np.asarray(t)
+#     t = np.reshape(t, input_dimensions)
+#
+#     learning_rate = 0.01
+#     t1 = convLayer1.forward(t)
+#
+#     t1 = np.reshape(t1, (t1.shape[1], t1.shape[2], t1.shape[0]))
+#     t2 = mxPoolLayer1.forward(t1)
+#     t2 = np.reshape(t2, (t2.shape[2], t2.shape[0], t2.shape[1]))
+#
+#     t3 = convLayer2.forward(t2)
+#
+#     t3 = np.reshape(t3, (t3.shape[1], t3.shape[2], t3.shape[0]))
+#     t4 = mxPoolLayer2.forward(t3)
+#     t4 = np.reshape(t4, (t4.shape[2], t4.shape[0], t4.shape[1]))
+#
+#     t5 = convLayer3.forward(t4)
+#
+#     t5 = np.reshape(t5, (t5.shape[1], t5.shape[2], t5.shape[0]))
+#     t6 = mxPoolLayer3.forward(t5)
+#     t6 = np.reshape(t6, (t6.shape[2], t6.shape[0], t6.shape[1]))
+#
+#     t7 = convLayer4.forward(t6)
+#
+#     t7 = np.reshape(t7, (t7.shape[1], t7.shape[2], t7.shape[0]))
+#     t8 = mxPoolLayer4.forward(t7)
+#     t8 = np.reshape(t8, (t8.shape[2], t8.shape[0], t8.shape[1]))
+#
+#     t9 = convLayer5.forward(t8)
+#
+#     t_final = flatten.forward(t9)
+#     processedFeatures[k] = t_final
 
 if __name__ == "__main__":
     processor = Processor()
@@ -77,3 +57,5 @@ if __name__ == "__main__":
     print("Pre Processing Images")
     processor.process_images()
     print("Images pre processed")
+
+    print(processor.data)
